5cbe0eb92443c4b8dc18d5c0eebe52dc0f393687
ravikant <Ravikantwaghmare@vionsys.com>
2024-12-26 18:12:34 +0530

    feat: Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,27 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
-  languageOptions: {
-    parserOptions: {
-      project: true,
-      tsconfigRootDir: import.meta.dirname,
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname
+        }
     },
-  },
-  files: ["**/*.ts"],
-  extends: [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    {
-      ignores: ["dist", "node_modules"],
-    },
-  ],
-  rules: {
-    "no-console": "error",
-    quotes: ["error", "single", { allowTemplateLiterals: true }],
-  },
+    files: ['**/*.ts'],
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        eslintConfigPrettier,
+        {
+            ignores: ['dist', 'node_modules']
+        }
+    ],
+    rules: {
+        'no-console': 'error',
+        quotes: ['error', 'single', { allowTemplateLiterals: true }]
+    }
 });
